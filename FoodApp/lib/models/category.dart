02ac9b6341519fc6240f52c9df37941b8824e3d7
ishon19 <pathak.shreyans@gmail.com>
2020-06-23 14:05:01 +0530

    Initial commit

---
+++ FoodApp/lib/models/category.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String id;
+  final String title;
+  final Color bgColor;
+
+  Category({
+    @required this.id,
+    @required this.title,
+    this.bgColor = Colors.lightGreen,
+  });
+}
