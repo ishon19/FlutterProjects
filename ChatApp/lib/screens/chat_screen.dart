7ce79442ad4c16d9eb268e00f240b26084b4c65a
ishon19 <pathak.shreyans@gmail.com>
2020-07-30 18:17:21 +0530

    firestore configured

---
+++ ChatApp/lib/screens/chat_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat App'),
+      ),
+      body: StreamBuilder(
+        builder: (ctx, snapShot) {
+          if (snapShot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = snapShot.data.documents;
+          return ListView.builder(
+            itemBuilder: (ctx, idx) => Container(
+              padding: EdgeInsets.all(8),
+              child: Text(documents[idx]['text']),
+            ),
+            itemCount: documents.length,
+          );
+        },
+        stream: Firestore.instance
+            .collection('chats/mxSrsFfwFeGycek1lW6D/messages')
+            .snapshots(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
