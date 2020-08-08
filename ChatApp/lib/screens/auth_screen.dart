844ea338b8e46a5ef37ff42f9fd87ddffc435ef6
ishon19 <pathak.shreyans@gmail.com>
2020-08-08 23:37:32 +0530

    firestore additional data logic added

--- ChatApp/lib/screens/auth_screen.dart
+++ ChatApp/lib/screens/auth_screen.dart
@@ -1,5 +1,8 @@
 import 'package:ChatApp/widgets/auth_form.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -7,11 +10,61 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  var isLoading = false;
+
+  void _submitAuthForm(
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+    BuildContext ctx,
+  ) async {
+    AuthResult authResult;
+
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        //sending additional data to firebase
+        Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData({
+          'username': username,
+          'email': email,
+        });
+      }
+    } on PlatformException catch (e) {
+      var msg = 'Some error occured, please check the login details';
+      print(e);
+      setState(() {
+        isLoading = false;
+      });
+      if (e.message != null) {
+        msg = e.message;
+      }
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(msg),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: AuthForm(),
+      body: AuthForm(_submitAuthForm, isLoading),
     );
   }
 }
--- ChatApp/lib/widgets/auth_form.dart
+++ ChatApp/lib/widgets/auth_form.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
+  AuthForm(
+    this.submitFn,
+    this.isLoading,
+  );
+
+  bool isLoading;
+
+  final void Function(
+    String email,
+    String password,
+    String userName,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
+
   @override
   _AuthFormState createState() => _AuthFormState();
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState.save();
+      widget.submitFn(
+        _userEmail.trim(),
+        _userPassword.trim(),
+        _userName.trim(),
+        _isLogin,
+        context,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -19,17 +56,46 @@ class _AuthFormState extends State<AuthForm> {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('email'),
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Invalid Email Address';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email Address',
                     ),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      onSaved: (value) {
+                        _userName = value;
+                      },
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4)
+                          return 'Username must be of 4 characters';
+                        return '';
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                      ),
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('password'),
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 6)
+                        return 'Password must be of 6 characters';
+                      return '';
+                    },
                     decoration: InputDecoration(
                       labelText: 'Password',
                     ),
@@ -38,13 +104,21 @@ class _AuthFormState extends State<AuthForm> {
                   SizedBox(
                     height: 12,
                   ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text('Login'),
+                  if(widget.isLoading) CircularProgressIndicator()
+                  else RaisedButton(
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'Login' : 'Signup'),
                   ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Text('Create new account'),
+                  if(!widget.isLoading) FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(_isLogin
+                        ? 'Already have an account?'
+                        : 'Create new account'),
                   ),
                 ],
               ),
